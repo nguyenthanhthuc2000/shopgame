16ebcaefc7c9c7594aa60729913d932027a8314b
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2024-11-22 01:08:32 +0700

    css

--- resources/views/components/slideshow.blade.php
+++ resources/views/components/slideshow.blade.php
@@ -5,13 +5,11 @@
                 <div class="col-lg-10 offset-lg-1 text-center">
                     <div class="hero-text">
                         <div class="hero-text-tablecell">
-                            <div class="hero-text-tablecell">
-                                <p class="subtitle animated fadeInUp" style="">Chất Lượng - Uy Tín - Hài Lòng</p>
-                                <h1 class="animated fadeInUp" style=" animation-delay: 0.3s;">NICKDAOQUAN.VN</h1>
-                                <p class="subdesc animated fadeInUp" style="">Hệ thống mua bán nick game uy tín số một Việt Nam!</p>
-                                <div class="hero-btns animated fadeInUp" style=" animation-delay: 0.5s;">
-                                    <a href="{{url()->current()}}#categories" class="boxed-btn">Xem Nick Ngay</a>
-                                </div>
+                            <p class="subtitle animated fadeInUp" style="">Chất Lượng - Uy Tín - Hài Lòng</p>
+                            <h1 class="animated fadeInUp" style=" animation-delay: 0.3s;">NICKDAOQUAN.VN</h1>
+                            <p class="subdesc animated fadeInUp" style="">Hệ thống mua bán nick game uy tín số một Việt Nam!</p>
+                            <div class="hero-btns animated fadeInUp" style=" animation-delay: 0.5s;">
+                                <a href="{{url()->current()}}#categories" class="boxed-btn">Xem Nick Ngay</a>
                             </div>
                         </div>
                     </div>
