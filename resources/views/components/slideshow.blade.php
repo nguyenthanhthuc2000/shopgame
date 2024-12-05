07a15e589a86f735cbe559ff427e3848f9c74c9e
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2024-12-05 22:24:07 +0700

    test

--- resources/views/components/slideshow.blade.php
+++ resources/views/components/slideshow.blade.php
@@ -1,6 +1,6 @@
 @if (isHomePage())
-    {{-- <div class="homepage-slider owl-carousel owl-theme">
-        <div class="single-homepage-slider homepage-bg-1">
+    <div class="homepage-slider owl-carousel owl-theme">
+        <div class="single-homepage-slider homepage-bg-1" style="background-image: url(http://test.nickdaoquan.vn/assets/images/homepage-bg-1.jpg);">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1 text-center">
@@ -19,8 +19,9 @@
                 </div>
             </div>
         </div>
-    </div> --}}
-    <div class="homepage-slider owl-carousel owl-theme">
+    </div>
+    <img src="http://test.nickdaoquan.vn/assets/images/homepage-bg-1.jpg" alt="">
+    {{-- <div class="homepage-slider owl-carousel owl-theme">
         <div class="single-homepage-slider">
             <img src="{{ asset('assets/images/homepage-bg-1.jpg') }}" alt="Banner 1" class="slider-img">
             <div class="container">
@@ -39,7 +40,6 @@
                     </div>
                 </div>
             </div>
-            <div></div>
         </div>
-    </div>
+    </div> --}}
 @endif
