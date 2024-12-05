87ece98c231b771829574fd2578f3723ca8c2ec8
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2024-12-05 23:58:09 +0700

    xx

--- resources/views/components/slideshow.blade.php
+++ resources/views/components/slideshow.blade.php
@@ -1,6 +1,6 @@
 @if (isHomePage())
     <div class="homepage-slider owl-carousel owl-theme">
-        <div class="single-homepage-slider homepage-bg-1">
+        <div class="single-homepage-slider homepage-bg-1" style="background-image: url({{ asset('assets/images/homepage-bg-1.jpg') }})">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-10 offset-lg-1 text-center">
