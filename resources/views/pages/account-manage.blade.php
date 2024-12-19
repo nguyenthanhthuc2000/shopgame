f6b5a9b0f2070a86ada0a31fe51fa4e9d9df18e7
NoTeNo <nhannguyen00974@gmail.com>
2024-12-19 23:49:32 +0700

    remove a

--- resources/views/pages/account-manage.blade.php
+++ resources/views/pages/account-manage.blade.php
@@ -74,9 +74,7 @@
                                                 <tbody>
                                                     @forelse($accounts as $account)
                                                         <tr>
-                                                            <td><a
-                                                                    href="{{ route('account.show', ['categorySlug' => $category->slug, 'accountUuid' => $account->uuid]) }}">#{{ $account->id }}</a>
-                                                            </td>
+                                                            <td>#{{ $account->id }}</td>
                                                             <td>{{ $account->username }}</td>
                                                             <td>{{ getPrice($account->price) }}</td>
                                                             <td>{{ getPrice($account->discount_price) }}</td>
@@ -158,7 +156,7 @@
                 error: function(error) {
                     Swal.fire({
                         title: 'Thông báo!',
-                        text: error.message || '{{ __('messages.common_error') }}',
+                        text: error.message || '{{ __("messages.common_error") }}',
                         icon: error.status || 'error',
                         confirmButtonColor: '#3085d6',
                         confirmButtonText: 'OK',
