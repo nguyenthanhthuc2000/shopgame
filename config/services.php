5c469ba963d2133aa0977ea58075b3a733a7d8ff
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2025-01-22 00:27:35 +0700

    buggggg

--- config/services.php
+++ config/services.php
@@ -18,11 +18,11 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
+    // 'ses' => [
+    //     'key' => env('AWS_ACCESS_KEY_ID'),
+    //     'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    //     'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    // ],
 
     'resend' => [
         'key' => env('RESEND_KEY'),
