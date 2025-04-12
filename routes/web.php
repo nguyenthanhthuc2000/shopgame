77e717479b5fe2fe5d3a8eb51bafbeacb6b2d1b2
Thức Nguyễn <nguyenthanhthuc.2k@gmail.com>
2025-04-12 09:49:23 +0700

    hi

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::middleware(['throttle:300,1'])->group(function () {
         Route::group(['middleware' => ['auth', IsAdminOrSeller::class]], function () {
             Route::get('/tai-khoan-da-ban', [AccountTransactionController::class, 'sellHistory'])->name('account.sell.history');
         });
+
         Route::group(['prefix' => 'quan-ly-nick-ngoc-rong', 'middleware' => ['auth', IsAdminOrSeller::class]], function () {
             Route::get('/', [AccountController::class, 'index'])->name('account.manage.index');
             Route::post('/', [AccountController::class, 'store'])->name('account.create.post');
